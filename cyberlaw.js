396d0cbdae143f617a5996772f822344a3d5257b
Justin Clark <jclark@cyber.law.harvard.edu>
2011-08-11 15:17:11 -0400

    twitter tweaks

--- cyberlaw.js
+++ cyberlaw.js
@@ -2,5 +2,9 @@ jQuery(document).ready(function() {
 	jQuery('#colophon').prepend(jQuery('#menu-primary > li > a').clone().each(function() { return jQuery(this).prop('outerHTML'); }));
 	jQuery('#rss-3 ul').cycle({
 		fx: 'scrollUp', // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+        timeout: 6000,
+        fit: 1,
+        width: 205,
+        pause: 1
 	});
 });
