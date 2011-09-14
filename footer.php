d6e798acccb064fcf3efed251ac4f165e7e70298
Justin Clark <jclark@cyber.law.harvard.edu>
2011-09-14 14:26:03 -0400

    added the footer sidebar

--- footer.php
+++ footer.php
@@ -28,19 +28,8 @@
 				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyten' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentyten' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s.', 'twentyten' ), 'WordPress' ); ?></a>
 			</div><!-- #site-generator -->
 
-        <div style="font-size: 11px;">
-            <div id="privacy" style="float:left; width:100px;"><a style="font-size:11px;" href="/about/privacy-policy">Privacy Policy</a></div>
-            <div id="footer-contact" style="float:right;width:100px;text-align:right"><a style="font-size:11px;" href="/contact">Contact</a></div>
-            <div id="street-address" style="text-align:center;">
-                23 Everett Street, 2nd Floor, Cambridge, MA 02138<br>
+            <div id="fullwidth-footer">
             </div>
-            <div id="telephone" style="text-align:center;">
-                +1 (617) 495-7547 (Phone)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+1 (617) 495-7641 (Fax)<br>
-            </div>
-            <div id="license" style="padding: 2px;text-align:center;">
-                Unless otherwise noted this site and its contents are licensed under a <a style="font-size: 11px; margin: 0;" href="http://creativecommons.org/licenses/by/3.0/" style="margin: 0">Creative Commons Attribution 3.0 Unported</a> license.
-            </div>
-        </div>
 		</div><!-- #colophon -->
 	</div><!-- #footer -->
 
--- functions.php
+++ functions.php
@@ -1,5 +1,13 @@
 <?php 
+function add_fullwidth_footer() {
+    $args = array(
+        'name'          => 'Full Width Footer',
+        'id'            => 'fullwidth-footer',
+        'description'   => 'The footer sidebar that takes up the full width of the page.');
+    register_sidebar($args);
+}
 wp_enqueue_script('jquery', 'jquery');
 wp_enqueue_script('jquerycycle', get_bloginfo('stylesheet_directory') . '/jquery.cycle.all.min.js');
 wp_enqueue_script('cyberlaw.js', get_bloginfo('stylesheet_directory') . '/cyberlaw.js');
+add_action('init', 'add_fullwidth_footer');
 ?>
