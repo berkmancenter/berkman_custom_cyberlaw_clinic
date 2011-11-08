be8263e5e9db1f08c96bfb39c353397de921dd29
Justin Clark <jclark@cyber.law.harvard.edu>
2011-11-08 14:08:04 -0500

    renamed posts and added shortcode

--- functions.php
+++ functions.php
@@ -7,8 +7,7 @@ function add_fullwidth_footer() {
     register_sidebar($args);
 }
 function add_custom_post_types() {
-
-    register_post_type('filings', array(
+    register_post_type('filing', array(
         'label' => 'Filings',
         'labels' => array('name' => 'Filings', 'singular_name' => 'Filing'),
         'description' => 'Filings and publications',
@@ -16,7 +15,7 @@ function add_custom_post_types() {
         'supports' => array('title', 'editor', 'page-attributes')
     ));
 
-    register_post_type('news_items', array(
+    register_post_type('news_item', array(
         'label' => 'News Items',
         'labels' => array('name' => 'News Items', 'singular_name' => 'News Item'),
         'description' => 'Occurences of the Clinic in the news',
@@ -24,7 +23,7 @@ function add_custom_post_types() {
         'supports' => array('title', 'editor', 'page-attributes')
     ));
 
-    register_post_type('upcoming_events', array(
+    register_post_type('upcoming_event', array(
         'label' => 'Upcoming Events',
         'labels' => array('name' => 'Upcoming Events', 'singular_name' => 'Upcoming Event'),
         'description' => 'Events that are up and coming',
@@ -32,7 +31,7 @@ function add_custom_post_types() {
         'supports' => array('title', 'editor', 'page-attributes')
     ));
 
-    register_post_type('past_events', array(
+    register_post_type('past_event', array(
         'label' => 'Past Events',
         'labels' => array('name' => 'Past Events', 'singular_name' => 'Past Event'),
         'description' => 'Events that are no longer in the future due to the passage of time',
@@ -40,7 +39,7 @@ function add_custom_post_types() {
         'supports' => array('title', 'editor', 'page-attributes')
     ));
 
-    register_post_type('staff_members', array(
+    register_post_type('staff_member', array(
         'label' => 'Staff Members',
         'labels' => array('name' => 'Staff Members', 'singular_name' => 'Staff Member'),
         'description' => 'Members of the Cyberlaw Clinic staff',
@@ -48,9 +47,53 @@ function add_custom_post_types() {
         'supports' => array('title', 'editor', 'page-attributes')
     ));
 }
+function custom_post_shortcode($atts) {
+	extract( shortcode_atts( array(
+        'type' => null,
+		'id' => null,
+		'count' => 'all',
+        'order' => 'ASC',
+        'orderby' => 'menu_order'
+	), $atts ) );
+    $args = array();
+
+    if (empty($type) && empty($id)) {
+        return '';
+    }
+
+    if ($count == 'all') {
+        $count = -1;
+    }
+
+    $contents = array();
+    if (!empty($id)) {
+        $args = array('p' => $id, 'post_type' => 'any');
+    }
+    else {
+        $args = array(
+          'post_type' => $type,
+          'post_status' => 'publish',
+          'posts_per_page' => $count,
+          'order' => $order,
+          'orderby' => $orderby
+        );
+    }
+    $my_query = new WP_Query($args);
+    if ( $my_query->have_posts() ) { 
+       while ( $my_query->have_posts() ) { 
+           $my_query->the_post();
+           $contents[] = get_the_content();
+       }
+    }
+    $html = implode('<hr />', $contents);
+    wp_reset_postdata();
+
+	return $html;
+}
 wp_enqueue_script('jquery', 'jquery');
 wp_enqueue_script('jquerycycle', get_bloginfo('stylesheet_directory') . '/jquery.cycle.all.min.js');
 wp_enqueue_script('cyberlaw.js', get_bloginfo('stylesheet_directory') . '/cyberlaw.js');
 add_action('init', 'add_fullwidth_footer');
 add_action('init', 'add_custom_post_types');
+add_shortcode( 'custom-post', 'custom_post_shortcode' );
 ?>
